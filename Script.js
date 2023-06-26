775c4399b43e1bb635ad3037a6e288449bee5a62
Mia Cruz <mia_cruz@dlsu.edu.ph>
2023-06-26 18:05:09 +0800

    commit

--- Script.js
+++ Script.js
@@ -29,6 +29,20 @@ $(window).on("scroll", function() {
 
 $(document).foundation();
 
+function search_title() {
+  let input = document.getElementsByClassName('searchbar').value
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName('post-title');
+
+  for (i = 0; i < x.length; i++) { 
+    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+        x[i].style.display="none";
+    }
+    else {
+        x[i].style.display="list-item";                 
+    }
+  }
+}
 
 function clickLike1() {
   let likebtn = document.getElementById("likebtn1");
