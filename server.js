f5c3d94224fd9a1e81211e55e0165bc1ac63afd4
Phoenix <134684094+PhoenixClaire@users.noreply.github.com>
2023-07-23 14:52:15 +0800

    server's working pero walang css properties TT

--- server.js
+++ server.js
@@ -1,36 +1,18 @@
 const dotenv = require('dotenv');
 const express = require('express');
 
+const controller = require('./controllers/controller.js');
 const app = express();
 
 dotenv.config();
 port = process.env.PORT;
 hostname = process.env.HOSTNAME;
 
+app.set('view engine', 'hbs');
+
 //home page
-app.get('/home', function(req, res){
-    res.send('Home Page');
-});
-
-app.get('/', function(req, res){
-    res.send('Home Page');
-});
-
-//my profile page
-app.get('/myprofile', function(req, res){
-    res.send('Profile Page');
-})
-
-//other profile
-app.get('/*viewprofile', function(req, res){
-    res.send('Other Profile Page');
-})
-
-//view post
-app.get('/*post', function(req, res){
-    res.send('Post');
-})
-
+app.get('/Yonde', controller.getYonde);
+app.get('/', controller.getYonde);
 
 app.listen(port,hostname, function(){
     console.log('Server running at: ');
