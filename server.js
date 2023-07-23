3a761e76e92f768229a811592dbd127f0be35c03
Phoenix <134684094+PhoenixClaire@users.noreply.github.com>
2023-07-23 14:06:23 +0800

    server update

--- server.js
+++ server.js
@@ -7,9 +7,30 @@ dotenv.config();
 port = process.env.PORT;
 hostname = process.env.HOSTNAME;
 
+//home page
+app.get('/home', function(req, res){
+    res.send('Home Page');
+});
+
 app.get('/', function(req, res){
     res.send('Home Page');
 });
+
+//my profile page
+app.get('/myprofile', function(req, res){
+    res.send('Profile Page');
+})
+
+//other profile
+app.get('/*viewprofile', function(req, res){
+    res.send('Other Profile Page');
+})
+
+//view post
+app.get('/*post', function(req, res){
+    res.send('Post');
+})
+
 
 app.listen(port,hostname, function(){
     console.log('Server running at: ');
